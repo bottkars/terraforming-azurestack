b54b0f3655640f8c181dc9bf20215107b74a6414
PCF Developer <pcfuser@labbuildr.com>
2019-06-14 18:02:22 +0200

    test

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "azurestack" {
 }
 
 # Create a resource group
-resource "azurestack_resource_group" "testrg" {
+resource "azurestack_resource_group" "test" {
   name     = "rg1"
   location = "${var.location}"
 }
 
 resource "azurestack_storage_account" "testsa" {
   name                     = "storageaccountname"
-  resource_group_name      = "${azurestack_resource_group.testrg.name}"
+  resource_group_name      = "${azurestack_resource_group.test.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -19,4 +19,22 @@ resource "azurestack_storage_account" "testsa" {
   tags = {
     environment = "staging"
   }
+}
+
+resource "azurestack_storage_container" "test" {
+  name                  = "vhds"
+  resource_group_name   = "${azurestack_resource_group.test.name}"
+  storage_account_name  = "${azurestack_storage_account.test.name}"
+  container_access_type = "private"
+}
+
+resource "azurestack_storage_blob" "testsb" {
+  name = "sample.vhd"
+
+  resource_group_name    = "${azurestack_resource_group.test.name}"
+  storage_account_name   = "${azurestack_storage_account.test.name}"
+  storage_container_name = "${azurestack_storage_container.test.name}"
+  source_uri = "https://opsmanagerimage.blob.westus.stackpoc.com/images/ops-manager-2.5.2-build.172.vhd"  
+  type = "page"
+  
 }
