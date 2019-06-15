f96c6227f0950b6e8dee506e9251c437089e12f6
PCF Developer <pcfuser@labbuildr.com>
2019-06-15 08:47:10 +0200

    optional

--- modules/ops_manager/ops_manager.tf
+++ modules/ops_manager/ops_manager.tf
@@ -164,8 +164,9 @@ resource "azurestack_virtual_machine" "optional_ops_manager_vm" {
   count                 = "${local.optional_ops_man_vm}"
   delete_os_disk_on_termination = "true"
   storage_os_disk {
-    name          = "opsman-disk.vhd"
-    vhd_uri       = "${azurestack_storage_blob.optional_ops_manager_image.url}"
+    name          = "optional-osdisk.vhd"
+    vhd_uri       = "${azurestack_storage_account.ops_manager_storage_account.primary_blob_endpoint}vhds/optional-osdisk.vhd"
+    image_uri     = "${azurestack_storage_blob.optional_ops_manager_image.url}"
     caching       = "ReadWrite"
     os_type       = "linux"
     create_option = "FromImage"
