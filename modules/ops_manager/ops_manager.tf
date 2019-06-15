8245a5a81ec31f2dbe306dfd23e138f6cf489474
PCF Developer <pcfuser@labbuildr.com>
2019-06-15 07:57:44 +0200

    disks

--- modules/ops_manager/ops_manager.tf
+++ modules/ops_manager/ops_manager.tf
@@ -22,7 +22,7 @@ resource "azurestack_storage_container" "ops_manager_storage_container" {
 }
 
 resource "azurestack_storage_blob" "ops_manager_image" {
-  name                   = "opsman-disk.vhd"
+  name                   = "opsman.vhd"
   resource_group_name    = "${var.resource_group_name}"
   storage_account_name   = "${azurestack_storage_account.ops_manager_storage_account.name}"
   storage_container_name = "${azurestack_storage_container.ops_manager_storage_container.name}"
@@ -30,7 +30,7 @@ resource "azurestack_storage_blob" "ops_manager_image" {
 }
 
 resource "azurestack_storage_blob" "optional_ops_manager_image" {
-  name                   = "optional_opsman-disk.vhd"
+  name                   = "optional_opsman.vhd"
   resource_group_name    = "${var.resource_group_name}"
   storage_account_name   = "${azurestack_storage_account.ops_manager_storage_account.name}"
   storage_container_name = "${azurestack_storage_container.ops_manager_storage_container.name}"
@@ -95,9 +95,10 @@ resource "azurestack_virtual_machine" "ops_manager_vm" {
 
   
   storage_os_disk {
-    name          = "opsman-disk.vhd"
-    image_uri     = "${azurestack_storage_account.ops_manager_storage_account}/vhds/osdisk.vhd"
-    vhd_uri     = "${azurestack_storage_blob.ops_manager_image.url}"
+    name          = "osdisk.vhd"
+    vhd_uri ="${azurestack_storage_account.ops_manager_storage_account.primary_blob_endpoint}${azurestack_storage_container.ops_manager_storage_container.name}/opsmandisk.vhd"
+  # vhd_uri     = "${azurestack_storage_account.ops_manager_storage_account.url}/vhds/osdisk.vhd"
+    image_uri     = "${azurestack_storage_blob.ops_manager_image.url}"
     caching       = "ReadWrite"
     os_type       = "linux"
     create_option = "FromImage"
