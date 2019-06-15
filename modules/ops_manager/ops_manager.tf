8ccf3a89db08546926f51e3f0e8cf591d6e4d4a9
PCF Developer <pcfuser@labbuildr.com>
2019-06-15 07:18:26 +0200

    opsmandisk

--- modules/ops_manager/ops_manager.tf
+++ modules/ops_manager/ops_manager.tf
@@ -22,7 +22,7 @@ resource "azurestack_storage_container" "ops_manager_storage_container" {
 }
 
 resource "azurestack_storage_blob" "ops_manager_image" {
-  name                   = "opsman.vhd"
+  name                   = "opsman-disk.vhd"
   resource_group_name    = "${var.resource_group_name}"
   storage_account_name   = "${azurestack_storage_account.ops_manager_storage_account.name}"
   storage_container_name = "${azurestack_storage_container.ops_manager_storage_container.name}"
@@ -95,7 +95,7 @@ resource "azurestack_virtual_machine" "ops_manager_vm" {
 
   
   storage_os_disk {
-    name          = "opsman-disk"
+    name          = "opsman-disk.vhd"
     vhd_uri       = "${azurestack_storage_blob.ops_manager_image.url}"
     caching       = "ReadWrite"
     os_type       = "linux"
