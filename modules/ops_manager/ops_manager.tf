70b37479a1986fd0c7f654e9c9c6ee7c9365ffd6
PCF Developer <pcfuser@labbuildr.com>
2019-06-15 07:42:16 +0200

    copy

--- modules/ops_manager/ops_manager.tf
+++ modules/ops_manager/ops_manager.tf
@@ -97,7 +97,7 @@ resource "azurestack_virtual_machine" "ops_manager_vm" {
   storage_os_disk {
     name          = "opsman-disk.vhd"
     image_uri     = "${azurestack_storage_account.ops_manager_storage_account}/vhds/osdisk.vhd"
-    vhd_uri     = "${var.source_img_uri}"
+    vhd_uri     = "${azurestack_storage_blob.ops_manager_image.url}"
     caching       = "ReadWrite"
     os_type       = "linux"
     create_option = "FromImage"
