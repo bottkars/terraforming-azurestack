b3bca1c2838508526bc51d21c1831173f06af56e
PCF Developer <pcfuser@labbuildr.com>
2019-06-17 14:50:28 +0200

    env short

--- modules/control_plane/variables.tf
+++ modules/control_plane/variables.tf
@@ -1,6 +1,7 @@
 variable "cidr" {}
 variable "dns_zone_name" {}
 variable "env_name" {}
+variable "env_short_name" {}
 variable "location" {}
 variable "resource_group_name" {}
 variable "network_name" {}
