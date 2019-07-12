f38b799955e0ba7f4f8de014bd55d8e08c8dd5a6
PCF Developer <pcfuser@labbuildr.com>
2019-07-12 13:18:40 +0200

    bumped to 0.8.1

--- local_tfvars.ps1
+++ local_tfvars.ps1
@@ -19,17 +19,17 @@ $env:TF_VAR_ops_manager_image_uri      = "https://opsmanagerimage.blob.local.azu
 
 
 
-export ARM_CLIENT_SECRET="m1;^y:1nV]%cM.s/]$]};_=["
-export ARM_SUBSCRIPTION_ID=57230479-98a0-4777-a763-8c024866a52a
+export ARM_CLIENT_SECRET="redacted"
+export ARM_SUBSCRIPTION_ID=redacted
 export ARM_ENDPOINT=https://management.local.azurestack.external
-export ARM_TENANT_ID=5f7dfed5-1a3d-424f-8e22-4661ae54b53b
-export ARM_CLIENT_ID=528621af-bbbb-44a8-bc62-3a2db31b0f62
+export ARM_TENANT_ID=redacted
+export ARM_CLIENT_ID=redacted
 export ARM_LOCATION=local
 
 
-export TF_VAR_ops_manager_image_uri=https://opsmanagerimage.blob.local.azurestack.external/images/ops-manager-2.5.4-build.189.vhd
-export TF_VAR_subscription_id=57230479-98a0-4777-a763-8c024866a52a
-export TF_VAR_client_secret="m1;^y:1nV]%cM.s/]$]};_=["
-export TF_VAR_tenant_id=5f7dfed5-1a3d-424f-8e22-4661ae54b53b
+export TF_VAR_ops_manager_image_uri=https://opsmanagerimage.blob.local.azurestack.external/images/ops-manager-2.6.3-build.163.vhd
+export TF_VAR_subscription_id=redacted
+export TF_VAR_client_secret=redacted
+export TF_VAR_tenant_id=redacted
 export TF_VAR_location=local
-export TF_VAR_client_id=528621af-bbbb-44a8-bc62-3a2db31b0f62
+export TF_VAR_client_id=redacted
